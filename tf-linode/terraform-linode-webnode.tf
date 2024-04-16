8c06325512952e5db0d012b93d49595692d42523
hammadrauf <rauf.hammad@gmail.com>
2024-04-16 14:41:15 -0400

    Update terraform-linode-webnode.tf

--- tf-linode/terraform-linode-webnode.tf
+++ tf-linode/terraform-linode-webnode.tf
@@ -38,6 +38,7 @@ resource "linode_instance" "webnode" {
         label = "debian-webnode"
         region = "us-east"
         type = "g6-nanode-1"
+        # Replace following with your SSH public keys, it is a list [].
         authorized_keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDrIccxxZq5CJwjUxjKnAr5z1c4cJjffrLFcR5tlv++eam5BXKJcM7dTMdm0xjDEq2aG2N5w5hjMICxOMeD4CU00hZzp35/ogAEDbBXQoQjEWYyfGJV37B/gVOhE4MEX+mPURUc18yFE30PoWoh5bEsQw8gmb2yeCWnNb8n1Guk9ZxmbEFhsus1M9vtcJjvpCyzKdhtcOJMjPMfWt5YIvxBwTrP9RFALut3ZU+LEv/9iNzujsE2yoznXW9BWDaFo9EODpVpu0a6Wg628RWkRry1VqsZoox2EHpAnEMCH/voWDcKyG0XhNMDfUi1mUoWdkJHIV5uPi4waUnnVSW96sCmd8XNchKLRUB5h/HR6X4TGYBMxN8N9ikFlBJL3qjQJcsg85f9TS183vJefmqrZ9sfl8t4BO+dDnSEUKCja9vE3egiqSnlqebnOlkclayTdOwOc8GbuGuGlOwBeL/h/7/eXoQ/lrNrmkEBfAe1Otjl9Omo1yFuwZALyb380OQGv2ZpdHCkbYJAMeS7h5tXF0+D0e1sTOY4f+OxkNgHfPkP3dfQuTm0Hw39tOpq+f0qjMSkIeN4+B48ZZuqstnOAlul7wMUxtNkUNgXBzduGw80LyhDkkwwGdi9zANKpKQMkTpqkj5JDvQgguqkUtrABuRcWb8FZ6SaIstSBRr2pe4TQ== rauf.hammad@gmail.com" ]
         root_pass = var.root_password
 
