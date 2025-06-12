e10ece5b78c8e7e35a189f9c43c2cc6dd6c5a6e1
Hammad Rauf <rauf.hammad@gmail.com>
2025-06-12 00:53:06 -0400

    Adding winServer2025 Terraform config

---
+++ tf-proxmox-winServer2025/README.md
@@ -0,0 +1,99 @@
+# WinServer2025 - Template - Notes
+Proxmox can store KVM VM images and LXC VM Images (CT templates) as template for quick deployment. LXC VMs share kernel with the Proxmox host, so only Linux is a possibility.
+
+## WinServer2025 - Minimum Requirements
+- Disk size - 32GB
+- RAM - 4 GB
+- TPM - true
+- Cores - Minimum 2 cores - 1 GHz or faster.
+
+
+## WinServer2025 - Sysprep
+[Refernce 1 - https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/sysprep--generalize--a-windows-installation?view=windows-11](https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/sysprep--generalize--a-windows-installation?view=windows-11)  
+[Reference 2 - https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/boot-windows-to-audit-mode-or-oobe?view=windows-11](https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/boot-windows-to-audit-mode-or-oobe?view=windows-11)  
+```cmd
+%WINDIR%\system32\sysprep\sysprep.exe /generalize /oobe /shutdown
+```
+  
+
+## Refernce Links
+1. [Poor Provider Docs: https://registry.terraform.io/providers/Telmate/proxmox/latest/docs](https://registry.terraform.io/providers/Telmate/proxmox/latest/docs)
+1. [Tutorial: https://spacelift.io/blog/terraform-proxmox-provider#4-configure-terraform-proxmox-provider](https://spacelift.io/blog/terraform-proxmox-provider#4-configure-terraform-proxmox-provider)
+1. [Good Provider Docs: https://registry.terraform.io/providers/bpg/proxmox/latest/docs](https://registry.terraform.io/providers/bpg/proxmox/latest/docs)
+
+## Setup On Proxmox:
+```
+pveum role add TerraformProv -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
+pveum user add terraform-prov@pve --password <password>
+pveum aclmod / -user terraform-prov@pve -role TerraformProv
+```
+## Create SSH User for Good Promox Provider (bpg/proxmox)
+```bash
+useradd terraform-prov -m
+passwd terraform-prov
+groupadd special
+usermod -a -G special,root terraform-prov
+nano /etc/sudoers.d/pvecommands
+cat /etc/sudoers.d/pvecommands
+## Cmnd alias specification
+Cmnd_Alias PVE_COMMANDS = /usr/sbin/qm
+#
+## Members of the special group may gain some privileges
+%special ALL=(ALL) NOPASSWD: PVE_COMMANDS
+
+root@pve:~#
+```
+
+## Windows Server 2025 - Template Creation
+
+1. Install Windows Server 2025
+
+1. Login as Administrator and Install 'virtio-win-gt-x64'
+    - This will install all Qemu drivers for Windows.
+
+1. Instal Qemu-Agent
+    a. Go to the mounted ISO in explorer
+    a. The guest agent installer is in the directory guest-agent
+    a. Execute the installer with double click (either qemu-ga-x86_64.msi (64-bit) or qemu-ga-i386.msi (32-bit)
+    After that the qemu-guest-agent should be up and running. You can validate this in the list of Window Services, or in a PowerShell with:
+    ```
+    PS C:\Users\Administrator> Get-Service QEMU-GA
+    
+    Status   Name               DisplayName
+    ------   ----               -----------
+    Running  QEMU-GA            QEMU Guest Agent
+```
+    If it is not running, you can use the Services control panel to start it and make sure that it will start automatically on the next boot.
+
+1. Install OpenSSH Server
+```
+PS C:\Users\Administrator> Add-WindowsCapability -Online -Name OpenSSH
+PS C:\Users\Administrator> Add-WindowsCapability -Online -Name OpenSSH.Server
+PS C:\Users\Administrator> Set-Service -Name sshd -StartupType Automatic
+PS C:\Users\Administrator> Set-Service -Name ssh-agent -StartupType Automatic
+PS C:\Users\Administrator> netsh advfirewall firewall add rule name="SSH Port" dir=in action=allow protocol=TCP localport=22 remoteip=any
+PS C:\Users\Administrator> Start-Service sshd
+PS C:\Users\Administrator> get-service sshd
+```
+
+1. Enable Remote RDP Sessions
+```
+PS C:\Users\Administrator> Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
+PS C:\Users\Administrator> Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+```
+
+1. Download Cloudbase-init Installer. Do not launch it yet.
+Download Link: [https://cloudbase.it/cloudbase-init/#download](https://cloudbase.it/cloudbase-init/#download)
+
+1. Shutdown the VM. Remove all CD Roms while stopped.
+
+1. Start the VM and Login again as Adminstrator. Temporarily release Network.
+```
+PS C:\Users\Administrator> ipconfig /release
+```
+
+1. Install Cloudbase-init as a Service, auto restart
+    - If Errors due to Packages, then remove that package and retry.
+    ```
+    PS C:\Users\Administrator> Remove-AppxPackage -Package Microsoft.WidgetsPlatformRuntime_1.6.1.0_x64__8wekyb3d8bbwe -allusers
+    ```
---
+++ tf-proxmox-winServer2025/tf-proxmox-winServer2025.tf
@@ -0,0 +1,274 @@
+# Adapted from: https://github.com/build-boxes/terraform-proxmox-windows-example/blob/main/main.tf
+#
+#
+# see https://github.com/hashicorp/terraform
+terraform {
+  required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/random
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+    # see https://registry.terraform.io/providers/hashicorp/cloudinit
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.6"
+    }
+    # see https://registry.terraform.io/providers/bpg/proxmox
+    # see https://github.com/bpg/terraform-provider-proxmox
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.75.0"
+    }
+  }
+}
+
+provider "proxmox" {
+    endpoint = var.PROXMOX_VE_ENDPOINT
+    username = var.PROXMOX_VE_USERNAME
+    password = var.PROXMOX_VE_PASSWORD
+    insecure = var.PROXMOX_VE_INSECURE
+  ssh {
+    agent = true
+    node {  
+      name    = var.proxmox_node_name
+      address = var.proxmox_node_address
+    }
+  }
+}
+
+variable "proxmox_node_name" {
+  type    = string
+  default = "pve"
+}
+
+variable "proxmox_node_address" {
+  type = string
+}
+
+variable "PROXMOX_VE_ENDPOINT" {
+    type = string
+    default = "https://192.168.4.20:8006/api2/json"
+}
+
+variable "PROXMOX_VE_USERNAME" {
+    type = string
+    sensitive = true
+    default = "admin@pve"
+}
+
+variable "PROXMOX_VE_PASSWORD" {
+    type = string
+    sensitive = true
+    default = "PassW0rd123!!"
+}
+
+variable "PROXMOX_VE_INSECURE" {
+    type = bool
+    default = false
+}
+
+
+variable "prefix" {
+  type    = string
+  default = "example-terraform-windows"
+}
+
+variable "username" {
+  type    = string
+  default = "terraform"
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+  # NB the password will be reset by the cloudbase-init SetUserPasswordPlugin plugin.
+  # NB this value must meet the Windows password policy requirements.
+  #    see https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/password-must-meet-complexity-requirements
+  default = "HeyH0Password"
+}
+
+# see https://registry.terraform.io/providers/bpg/proxmox/0.75.0/docs/data-sources/virtual_environment_vms
+data "proxmox_virtual_environment_vms" "windows_templates" {
+  tags = ["windows", "template", "winserver", "2025"]
+}
+
+# see https://registry.terraform.io/providers/bpg/proxmox/0.75.0/docs/data-sources/virtual_environment_vm
+data "proxmox_virtual_environment_vm" "windows_template" {
+  node_name = data.proxmox_virtual_environment_vms.windows_templates.vms[0].node_name
+  vm_id     = data.proxmox_virtual_environment_vms.windows_templates.vms[0].vm_id
+}
+
+# the virtual machine cloudbase-init cloud-config.
+# NB the parts are executed by their declared order.
+# see https://github.com/cloudbase/cloudbase-init
+# see https://cloudbase-init.readthedocs.io/en/1.1.6/userdata.html#cloud-config
+# see https://cloudbase-init.readthedocs.io/en/1.1.6/userdata.html#userdata
+# see https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/config.html
+# see https://developer.hashicorp.com/terraform/language/expressions#string-literals
+data "cloudinit_config" "example" {
+  gzip          = false
+  base64_encode = false
+  part {
+    filename     = "initialize-disks.ps1"
+    content_type = "text/x-shellscript"
+    content      = <<-EOF
+      #ps1_sysnative
+      # initialize all (non-initialized) disks with a single NTFS partition.
+      # NB we have this script because disk initialization is not yet supported by cloudbase-init.
+      # NB the output of this script appears on the cloudbase-init.log file when the
+      #    debug mode is enabled, otherwise, you will only have the exit code.
+      Get-Disk `
+        | Where-Object {$_.PartitionStyle -eq 'RAW'} `
+        | ForEach-Object {
+          Write-Host "Initializing disk #$($_.Number) ($($_.Size) bytes)..."
+          $volume = $_ `
+            | Initialize-Disk -PartitionStyle MBR -PassThru `
+            | New-Partition -AssignDriveLetter -UseMaximumSize `
+            | Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk$($_.Number)" -Confirm:$false
+          Write-Host "Initialized disk #$($_.Number) ($($_.Size) bytes) as $($volume.DriveLetter):."
+        }
+      EOF
+  }
+  part {
+    content_type = "text/cloud-config"
+    content      = <<-EOF
+      #cloud-config
+      hostname: example
+      timezone: America/Toronto
+      users:
+        - name: ${jsonencode(var.username)}
+          passwd: ${jsonencode(var.password)}
+          primary_group: Administrators
+          ssh_authorized_keys:
+            - ${jsonencode(trimspace(file("~/.ssh/id_rsa.pub")))}
+      # these runcmd commands are concatenated together in a single batch script and then executed by cmd.exe.
+      # NB this script will be executed as the cloudbase-init user (which is in the Administrators group).
+      # NB this script will be executed by the cloudbase-init service once, but to be safe, make sure its idempotent.
+      # NB the output of this script appears on the cloudbase-init.log file when the
+      #    debug mode is enabled, otherwise, you will only have the exit code.
+      runcmd:
+        - "echo # Script path"
+        - "echo %~f0"
+        - "echo # Sessions"
+        - "query session"
+        - "echo # whoami"
+        - "whoami /all"
+        - "echo # Windows version"
+        - "ver"
+        - "echo # Environment variables"
+        - "set"
+      EOF
+  }
+  part {
+    filename     = "example.ps1"
+    content_type = "text/x-shellscript"
+    content      = <<-EOF
+      #ps1_sysnative
+      # this is a PowerShell script.
+      # NB this script will be executed as the cloudbase-init user (which is in the Administrators group).
+      # NB this script will be executed by the cloudbase-init service once, but to be safe, make sure its idempotent.
+      # NB the output of this script appears on the cloudbase-init.log file when the
+      #    debug mode is enabled, otherwise, you will only have the exit code.
+      Start-Transcript -Append "C:\cloudinit-config-example.ps1.log"
+      function Write-Title($title) {
+        Write-Output "`n#`n# $title`n#"
+      }
+      Write-Title "Script path"
+      Write-Output $PSCommandPath
+      Write-Title "Sessions"
+      query session | Out-String
+      Write-Title "whoami"
+      whoami /all | Out-String
+      Write-Title "Windows version"
+      cmd /c ver | Out-String
+      Write-Title "Environment Variables"
+      dir env:
+      Write-Title "TimeZone"
+      Get-TimeZone
+      EOF
+  }
+}
+
+# see https://registry.terraform.io/providers/bpg/proxmox/0.75.0/docs/resources/virtual_environment_file
+resource "proxmox_virtual_environment_file" "example_ci_user_data" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.proxmox_node_name
+  source_raw {
+    file_name = "${var.prefix}-ci-user-data.txt"
+    data      = data.cloudinit_config.example.rendered
+  }
+}
+
+# see https://registry.terraform.io/providers/bpg/proxmox/0.75.0/docs/resources/virtual_environment_vm
+resource "proxmox_virtual_environment_vm" "example" {
+  name      = var.prefix
+  node_name = var.proxmox_node_name
+  tags      = sort(["windows-2025-uefi", "example", "terraform"])
+  clone {
+    vm_id = data.proxmox_virtual_environment_vm.windows_template.vm_id
+    full  = true
+  }
+  cpu {
+    #type  = "host"
+    type  = "x86-64-v2-AES"
+    cores = 2
+  }
+  memory {
+    dedicated = 4 * 1024
+  }
+  network_device {
+    bridge = "vmbr0"
+  }
+  disk {
+    interface   = "scsi0"
+    file_format = "raw"
+    iothread    = true
+    ssd         = true
+    discard     = "on"
+    size        = 40
+  }
+  disk {
+    interface   = "scsi1"
+    file_format = "raw"
+    iothread    = true
+    ssd         = true
+    discard     = "on"
+    size        = 16
+  }
+  agent {
+    enabled = true
+    trim    = true
+  }
+  # NB we use a custom user data because this terraform provider initialization
+  #    block is not entirely compatible with cloudbase-init (the cloud-init
+  #    implementation that is used in the windows base image).
+  # see https://pve.proxmox.com/wiki/Cloud-Init_Support
+  # see https://cloudbase-init.readthedocs.io/en/latest/services.html#openstack-configuration-drive
+  # see https://registry.terraform.io/providers/bpg/proxmox/0.75.0/docs/resources/virtual_environment_vm#initialization
+  initialization {
+    user_data_file_id = proxmox_virtual_environment_file.example_ci_user_data.id
+  }
+  # NB this can only connect after about 3m15s (because the ssh service in the
+  #    windows base image is configured as "delayed start").
+  provisioner "remote-exec" {
+    connection {
+      target_platform = "windows"
+      type            = "ssh"
+      host            = self.ipv4_addresses[index(self.network_interface_names, "Ethernet")][0]
+      user            = var.username
+      password        = var.password
+    }
+    # NB this is executed as a batch script by cmd.exe.
+    inline = [
+      <<-EOF
+      whoami.exe /all
+      EOF
+    ]
+  }
+}
+
+output "ip" {
+  value = proxmox_virtual_environment_vm.example.ipv4_addresses[index(proxmox_virtual_environment_vm.example.network_interface_names, "Ethernet")][0]
+}
